ec33f031fff5c681fc9940fa364c927b3b8def2d
Alex Gorodov <a.gorodov.22@gmail.com>
2022-12-17 00:21:22 +0200

    modals transition

--- src/js/contact.js
+++ src/js/contact.js
@@ -9,16 +9,20 @@ submit.onclick = function () {
     form.addEventListener('submit', function () {
       success.classList.add('modal--show');
       setTimeout(() => {
+        success.style.transition = '0.5s';
         success.classList.remove('modal--show');
       }, 5000);
+      success.style.transition = 'cubic-bezier(0, 0.7, 0.2, 1.1) 0.5s';
     });
     for (let i = 0; i < inputs.length; i++) {
       if (!inputs[i].validity.valid) {
             labels[i].classList.add('contact__item--invalid');
             error.classList.add('modal--show');
             setTimeout(() => {
+              error.style.transition = '0.5s';
               error.classList.remove('modal--show');
             }, 5000);
+            error.style.transition = 'cubic-bezier(0, 0.7, 0.2, 1.1) 0.5s';
         } else {
           labels[i].classList.remove('contact__item--invalid');
         }
